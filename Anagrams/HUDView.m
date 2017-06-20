ea33f3f342860d247bae73f5bf52ac49d5c48046
Shivani <shivani@Shivanis-MacBook-Air.local>
2017-06-20 16:50:21 +0530

    Added Hint Button to HUD (action not implemented )

--- Anagrams/HUDView.m
+++ Anagrams/HUDView.m
@@ -28,13 +28,13 @@
     hudView.userInteractionEnabled = NO ;
     
     // adding the stopwatch
-    hudView.stopwatch = [[StopwatchView alloc] initWithFrame:CGRectMake(hudView.center.x/2 , 0 , 300, 100)] ;
+    hudView.stopwatch = [[StopwatchView alloc] initWithFrame:CGRectMake(hudView.center.x-50 , 0 , 300, 100)] ;
     [hudView.stopwatch setSeconds:10];
     [hudView addSubview:hudView.stopwatch];
     
     // adding the points label
     
-    UILabel *pointsLabel = [[UILabel alloc] initWithFrame:CGRectMake(hudView.center.x+hudView.center.x/2,30,140,70)];
+    UILabel *pointsLabel = [[UILabel alloc] initWithFrame:CGRectMake(hudView.stopwatch.frame.origin.x + hudView.stopwatch.frame.size.width+50,30,140,70)];
     pointsLabel.backgroundColor = kClearColor ;
     pointsLabel.font =kFontHUD ;
     pointsLabel.text = pointsLabelText;
@@ -46,11 +46,29 @@
     hudView.gamePoints.textColor = kGamePointsColor ;
     
     [hudView addSubview: hudView.gamePoints];
+    
+    UIImage *buttonImage = [UIImage imageNamed:@"btn"];
+    
+    hudView.helpButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [hudView.helpButton setTitle:@"Hint!" forState:UIControlStateNormal] ;
+    hudView.helpButton.titleLabel.font = kFontHUD;
+    [hudView.helpButton setBackgroundImage:buttonImage forState:UIControlStateNormal];
+    [hudView.helpButton addTarget:self action:@selector(hintButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    
+    hudView.helpButton.frame = CGRectMake(30, 30, buttonImage.size.width, buttonImage.size.height);
+     hudView.helpButton.alpha = 0.8 ;
+     [hudView addSubview:hudView.helpButton];
+
 
     
     return hudView ;
 
 }
 
+-(IBAction)hintButtonPressed:(id)sender
+{
+    NSLog(@" hint button pressed ");
+    //TODO : implement button action 
+}
 
 @end
